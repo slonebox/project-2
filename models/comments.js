6ff7a963ff5f9ab3c82b38ca536b96484bdd4d31
William Mooney Sloneker <wmsloneker@gmail.com>
2019-10-31 21:47:20 -0400

    Developed models for the bootiqueDB

---
+++ models/comments.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes) {
+  var Comments = sequelize.define("Comments", {
+    replyContent: DataTypes.TEXT
+  });
+
+  //Associates replies under a specific account
+  Comments.associate = function(models) {
+    Comments.belongsTo(models.Users, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  //Associates replies under a specific post
+  Comments.associate = function(models) {
+    Comments.belongsTo(models.Posts, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Comments;
+};
---
+++ models/posts.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+  var Posts = sequelize.define("Posts", {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT
+  });
+
+  //Associates posts under a specific account
+  Posts.associate = function(models) {
+    Posts.belongsTo(models.Users, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  //Associates various replies to a post
+  Posts.associate = function(models) {
+    Posts.hasMany(models.Comments, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Posts;
+};
